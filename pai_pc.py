fea3793af0e9bbed3453d11aafeab666b8de290e
yinzhixin <13681028312@163.com>
2016-04-14 16:20:52 +0800

    test2

--- pai_pc.py
+++ pai_pc.py
@@ -15,6 +15,10 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoSuchFrameException
+'''
+loadTestsFromTestCasetest
+testtestsetsetset
+'''
 
 siturl = "http://paisit.cnsuning.com/shanpai/"
 items = []
